74bbb4be63ca7eacaf75af52e6bb1f498e960c9e
codevault01729 <157150096+codevault01729@users.noreply.github.com>
2024-08-13 12:20:44 +0000

    datatypes_done

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,40 @@
+// Primitive
+
+// 7types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+
+const temp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 2343445534234342343n
+
+const heroes = ["shaktiman", "flying jatt", "doga"] //arrays
+
+const dataCustomer ={
+    name : "shakya",
+    age : 12,
+    isLoggedIn : true
+}
+
+console.log(dataCustomer);
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+myFunction()
+
+console.log(typeof bigNumber);
+
+
+// Reference (Non Primitve)
+
+//Arrays, Objects, Functions
